1e2fade3dc9e8c5c608350899005ceb35b09f398
Dresdain <dresdain@live.com>
2019-06-30 02:05:56 +0800

    Revised to have better logic

--- 5x5/demo.js
+++ 5x5/demo.js
@@ -1,89 +1,93 @@
-// Image API
+// Unsplash Image API
 var unsplashAPI = 'https://source.unsplash.com/120x150/?';
 var keywords = [
     'nature', 'water', 'city', 'night', 'fruit', 'sunset', 'music', 'town', 'lights', 'space', 'games', 'tea', 'drinks', 'coffee', 'trees', 'football', 'ocean', 'bicycle', 'taxi', 'painting', 'hospital', 'fashion', 'travel', 'people', 'health', 'culture', 'garden', 'computer', 'fire', 'camping', 'mountain', 'lake', 'meeting', 'building', 'japan', 'snow', 'bokeh'
 ]; 
 
-// Setup delay
-var delay = 1000; 
-var slideCounter = 0;
-// Random Tiles 
-var maxTiles = 25; 
-var randomImageSelect = randomUniqueInt(0, keywords.length, 25);
-console.log(randomImageSelect);
-
-var randomTileToChange = randomUniqueInt(0, 24, 25);
-  
-
-// 👇 Uncomment the following to stop randomization
-// clearInterval(randomizeTiles);
-// clearInterval(randomizeImages);
-
-// 👇 Set the following to bool false, to disable slideshow
-var slideShow = true;
-
-
-
+var delay = 10000;  // Setup delay interval 1000ms = 1second
+var maxTiles = 25; // Set maximum tiles to show and randomize
+ 
 $(window).on("load", function () {
-    
-    $(".tile").each(function(index){
-        // Show only maxTiles
+    // The following loop, populates the first set of random images, limits the number of tiles to var maxTiles
+    var randomImageSelect = getRandomUniqueInt(0, keywords.length, 25);
+    $(".tile").each(function(index){ 
         if (index >= maxTiles){ $(this).hide() }
-        if (index < maxTiles){
-            // Using unsplash API involves adding keywords to the url
-            // We check if keyword index is within our [keywords] array
-            // otherwise, if index > keywords.length 
-            // we will use a 'random' keyword as fallback
+        if (index < maxTiles){ 
+            //Check if a keyword exist in the keywords array, else we use the keyword 'random'
             var getImageKeyword = (keywords[randomImageSelect[index]] ? keywords[randomImageSelect[index]] : 'random');
             $(this).append('<img>')
                 .children('img')
                 .attr('src', unsplashAPI + getImageKeyword);
-            $(this).append('<img/>');  
         }
     });
-
-    
-    // Store the next img in a hidden img tag
-    preloadImg();
-    var preloadimg_interval = setInterval(preloadImg, delay * (maxTiles - 1));
-
-    //Randomly pick a tile, hide first img, show second image
-    console.log(randomTileToChange);
-    
-    var randomizedTiles_interval = setInterval(() => {  
-        $.when($('.tile').eq(randomTileToChange[slideCounter]).children('img:nth-child(1)').fadeOut('500'))
-            .done(function(){
-                $(this).remove();
-                $('.tile').eq(randomTileToChange[slideCounter]).children('img:nth-child(2)').fadeIn('500');
-               
-                 $('.tile').eq(randomTileToChange[slideCounter]).append('<img>');
-            });
-        
-        
-        console.log(randomTileToChange[slideCounter]);    
-        
-        (slideCounter < maxTiles - 1 ? slideCounter++ : slideCounter = 0); 
-    }, delay);  
-
- 
+    // Start the cycle
+    randomizeTiles();
 }); 
 
-function preloadImg(){
-    randomImageSelect = randomUniqueInt(0, keywords.length, 25);
-    // console.log(randomImageSelect);
-    $(".tile").each(function (index) {
-         $('.tile').eq(randomTileToChange[slideCounter]).append('<img>');
-        //Initialize next image tag
-        var getImageKeyword = (keywords[randomImageSelect[index]] ? keywords[randomImageSelect[index]] : 'random');
-        $(this).children().eq(1)
-            .hide()
-            .attr('src', unsplashAPI + getImageKeyword);
-    });
+function randomizeTiles() {
+    //Select a random tile
+    var selectedRandomNum  = getRandomInt(0, (maxTiles - 1));
+    var selectedRandomTile = $('.tile').eq(selectedRandomNum);
+
+    //Check if delayOffset is not greater than the delay intended
+    //Fallback to no delay / 0ms if false
+    var delayOffset = (delay > delay - 1000 ? (delay - 1000) : 0);
+    
+    //Ready next image URL
+    var nextImage = unsplashAPI + keywords[getRandomInt(0, keywords.length)];
+   
+    //Make sure there is no duplicate, change src if there is
+    while (checkForDuplicate(nextImage) === true) {
+        nextImage = unsplashAPI + keywords[getRandomInt(0, keywords.length)];
+    }
+    if(checkForDuplicate(nextImage) === false){
+        selectedRandomTile.append('<img src="' + nextImage+ '" style="display: none;" />');
+    }
+
+    //Delay fadeout and subtract offset to get a total of desired interval
+    setTimeout(() => { 
+        // $.when is a Promise and may require polyfill on IE
+        $.when(selectedRandomTile.children('img:nth-child(1)').fadeOut())
+            .done(function () {
+                // Reset the cycle by removing original image, fading in new one
+                selectedRandomTile.children('img:nth-child(1)').remove();
+                selectedRandomTile.children('img:nth-child(1)').fadeIn(); 
+                console.log('Changing tile #: ' + (selectedRandomNum+1));
+                // And cycle again
+                setTimeout(randomizeTiles, delay - delayOffset);
+            });
+    }, delayOffset);
+}
+
+
+/* Traverses all .tiles > img in the DOM and checks for a duplicate of nextImage in their src attribute */
+function checkForDuplicate(nextImage){
+    var duplicate = false;
+    $('.tile > img').map(function (index) {
+        if (nextImage == $(this).attr('src')) {
+            duplicate = true; 
+        }
+    }); 
+    return (duplicate === true ? true : false);
+}
+
+
+/**
+ * Returns a random integer between min (inclusive) and max (inclusive).
+ * The value is no lower than min (or the next integer greater than min
+ * if min isn't an integer) and no greater than max (or the next integer
+ * lower than max if max isn't an integer).
+ * Using Math.round() will give you a non-uniform distribution!
+ */
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 
 /* Converted to ES5, original: https://stackoverflow.com/questions/52528059/javascript-generate-a-random-number-between-1-and-5-but-never-the-same-number */
-function randomUniqueInt(min, max, length){ 
+function getRandomUniqueInt(min, max, length){ 
     var limit = max - min + 1;
     if (min > max || max < min) {
         throw new Error(`Parameter "min" has to be smaller than "max" and vice-versa.`)
