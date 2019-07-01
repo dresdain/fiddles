7528b21f8a5bc82a8d5e4fc7bed5c03f86eebe8c
Dresdain <dresdain@live.com>
2019-07-01 13:21:49 +0800

    Added local images + updated logic

--- 5x5/demo.js
+++ 5x5/demo.js
@@ -1,16 +1,49 @@
 // Unsplash Image API
-var unsplashAPI = 'https://source.unsplash.com/120x150/?';
-var keywords = [
-    'nature', 'water', 'city', 'night', 'fruit', 'sunset', 'music', 'town', 'lights', 'space', 'games', 'tea', 'drinks', 'coffee', 'trees', 'football', 'ocean', 'bicycle', 'taxi', 'painting', 'hospital', 'fashion', 'travel', 'people', 'health', 'culture', 'garden', 'computer', 'fire', 'camping', 'mountain', 'lake', 'meeting', 'building', 'japan', 'snow', 'bokeh'
+var rootDirectory = './img/';
+var images = [
+    'stock-photo-1.jpeg',
+    'stock-photo-2.jpeg',
+    'stock-photo-3.jpeg',
+    'stock-photo-4.jpeg',
+    'stock-photo-5.jpeg',
+    'stock-photo-6.jpeg',
+    'stock-photo-7.jpeg',
+    'stock-photo-8.jpeg',
+    'stock-photo-9.jpeg',
+    'stock-photo-10.jpeg',
+    'stock-photo-11.jpeg',
+    'stock-photo-12.jpeg',
+    'stock-photo-13.jpeg',
+    'stock-photo-14.jpeg',
+    'stock-photo-15.jpeg',
+    'stock-photo-16.jpeg',
+    'stock-photo-17.jpeg',
+    'stock-photo-18.jpeg',
+    'stock-photo-19.jpeg',
+    'stock-photo-20.jpeg',
+    'stock-photo-21.jpeg',
+    'stock-photo-22.jpeg',
+    'stock-photo-23.jpeg',
+    'stock-photo-24.jpeg',
+    'stock-photo-25.jpeg',
+    'stock-photo-26.jpeg',
+    'stock-photo-27.jpeg',
+    'stock-photo-28.jpeg',
+    'stock-photo-29.jpeg',
+    'stock-photo-30.jpeg',
+    'stock-photo-31.jpeg',
+    'stock-photo-32.jpeg',
+    'stock-photo-33.jpeg',
+    'stock-photo-34.jpeg',
+    'stock-photo-35.jpeg'
 ]; 
 
 //Select container and child
 var tileContainer = ".tile-container";
 var tileName = ".tile";
 
-var delay = 5;  // Set delay interval in seconds
+var delay = 10;  // Set delay interval in seconds
 var maxTiles = 25; // Set maximum tiles to show and randomize
-
 
 var tileArticle = tileContainer + ' > ' + tileName;
 $(window).on("load", function () {
@@ -21,17 +54,19 @@ $(window).on("load", function () {
         }
     }
 
+    var imageTotal = (images.length < maxTiles ? maxTiles - images.length : maxTiles);
+    var imageSetTotal = (images.length < maxTiles ? images.length : maxTiles);  
+    var randomImageSelect = getRandomUniqueInt(0, imageTotal, imageSetTotal);
+    console.log(randomImageSelect);
+
     // The following loop, populates the first set of random images, limits the number of tiles to var maxTiles
-    var randomImageSelect = getRandomUniqueInt(0, keywords.length, maxTiles);
     $(tileArticle).each(function(index){ 
         if (index >= maxTiles){ $(this).hide() }
         if (index < maxTiles){ 
-            $(this).addClass('randomize');
-            //Check if a keyword exist in the keywords array, else we use the keyword 'random'
-            var getImageKeyword = (keywords[randomImageSelect[index]] ? keywords[randomImageSelect[index]] : 'random');
+            $(this).addClass('randomize');   
             $(this).append('<img>')
                 .children('img')
-                .attr('src', unsplashAPI + getImageKeyword)
+                .attr('src', rootDirectory + images[randomImageSelect[index]])
                 .hide()
                 .fadeIn();
         }
@@ -44,22 +79,23 @@ function randomizeTiles() {
     //Select a random tile
     var selectedRandomNum  = getRandomInt(0, (maxTiles - 1));
     var selectedRandomTile = $('.tile').eq(selectedRandomNum);
-
+    
     //Check if delayOffset is not greater than the delay intended
     //Fallback to no offsetDelay / 0ms if false aka not preloading the image
     var convertDelay = delay * 1000;
     var delayOffset = (convertDelay > convertDelay - 1000 ? (convertDelay - 1000) : 0);
     
     //Ready next image URL
-    var nextImage = unsplashAPI + keywords[getRandomInt(0, keywords.length)];
+    var nextImage = rootDirectory + images[getRandomInt(0, images.length - 1)];
    
     //Make sure there is no duplicate, change src if there is
     while (checkForDuplicate(nextImage) === true) {
-        nextImage = unsplashAPI + keywords[getRandomInt(0, keywords.length)];
+        nextImage = rootDirectory + images[getRandomInt(0, images.length - 1)];
     }
     if(checkForDuplicate(nextImage) === false){
-        selectedRandomTile.append('<img src="' + nextImage+ '" style="display: none;" />');
-    }
+        var newImage = '<img src="' + nextImage + '" style="display: none;" />';
+        selectedRandomTile.append(newImage); 
+    } 
 
     //Delay fadeout and subtract offset to get a total of desired interval
     setTimeout(() => { 
@@ -71,9 +107,10 @@ function randomizeTiles() {
                 selectedRandomTile.children('img:nth-child(1)').fadeIn(); 
                 console.log('Changing tile #: ' + (selectedRandomNum+1));
                 // And cycle again
-                setTimeout(randomizeTiles, convertDelay - delayOffset);
+                randomImageCycle = setTimeout(randomizeTiles, convertDelay - delayOffset);
             });
     }, delayOffset);
+ 
 }
  
 /* Traverses all .tiles > img in the DOM and checks for a duplicate of nextImage in their src attribute */
